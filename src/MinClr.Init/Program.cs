489e67ec5cdc0c917df3f3a610052253556ca50c
MouriNaruto <Mouri_Naruto@outlook.com>
2021-03-21 23:20:51 +0800

    Start to implement the MinClr.Init project.

--- src/MinClr.Init/Program.cs
+++ src/MinClr.Init/Program.cs
@@ -8,14 +8,125 @@ namespace MinClr.Init
 {
     class Program
     {
+        [DllImport("libc")]
+        private static extern int fork();
+
+        [DllImport("libc")]
+        private static extern int execve(
+            string pathname,
+            string[] argv,
+            string[] envp);
+
+        [DllImport("libc")]
+        private static extern void _exit(
+            int status);
+
+        private static int CreateProcessFromInit(
+            ProcessStartInfo startInfo)
+        {
+            int pid = fork();
+
+            if (pid == 0)
+            {
+                Directory.SetCurrentDirectory(startInfo.WorkingDirectory);
+
+                List<string> envp = new List<string>();
+                foreach (var item in startInfo.Environment)
+                {
+                    envp.Add(string.Format("{0}={1}", item.Key, item.Value));
+                }
+                envp.Add(null);
+
+                string[] argv = new string[] { "/usr/bin/bash", null };
+
+                execve(argv[0], argv, envp.ToArray());
+
+                // If failed to call execve, it will return the errno.
+                _exit(Marshal.GetLastWin32Error());
+            }
+
+            return pid;
+        }
+
+
+        [DllImport("libc")]
+        private static extern int mount(
+            string source, 
+            string target,
+            string filesystemtype,
+            ulong mountflags, 
+            string data);
+
+        private const ulong MS_REMOUNT = 32;
+
+        [DllImport("libc")]
+        private static extern int umount2(
+            string target, 
+            int flags);
+
+        private const int MNT_DETACH = 2;
+
+
+
+
         static void Main(string[] args)
         {
             Console.Clear();
 
             Console.WriteLine(
-                "Hello World from init process based on .NET Runtime.");
+                "MinClr.Init for .NET/Linux 0.1.1");
+            Console.WriteLine(
+                "(c) .NET/Linux Development Team. All rights reserved.");
+            Console.WriteLine("");
 
-            Console.ReadKey();
+            Console.WriteLine("The version of .NET/Linux: 0.1.2");
+            Console.WriteLine("");
+
+            Console.WriteLine("Make / writable.");
+            if (-1 == mount(null, "/", null, MS_REMOUNT, null))
+            {
+                Console.WriteLine("Failed to make / writable.");
+            }
+
+            Console.WriteLine("Detach the /tmp mount point from the ram disk.");
+            if (-1 == umount2("/tmp", MNT_DETACH))
+            {
+                Console.WriteLine(
+                    "Failed to detach the /tmp mount point from the ram disk.");
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Starting DHCP deamon...");
+
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(
+                    "/usr/sbin/dhcpcd");
+                Process.Start(startInfo).WaitForExit();
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Starting PowerShell...");
+            for (;;)
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo(
+                        "/opt/minclr/powershell/pwsh",
+                        "-NoLogo");
+                    string OldPath = startInfo.Environment["PATH"];
+                    startInfo.Environment["PATH"] = 
+                        "/opt/minclr/powershell:" + OldPath;
+                    startInfo.Environment["HOME"] = "/root";
+                    startInfo.WorkingDirectory = startInfo.Environment["HOME"];
+                    Process.Start(startInfo).WaitForExit();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Failed to start PowerShell. ({0})", e.Message);
+                }
+            }
         }
     }
 }
