44d29b6482dc6f682a05443740dcda1f7b4f19df
MouriNaruto <Mouri_Naruto@outlook.com>
2021-03-21 23:20:07 +0800

    Add MinClr.Init project.

---
+++ src/MinClr.Init/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace MinClr.Init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine(
+                "Hello World from init process based on .NET Runtime.");
+
+            Console.ReadKey();
+        }
+    }
+}
