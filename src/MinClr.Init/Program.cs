1d0377b7d32d5cd5dccedece789d55f2bce840d1
MouriNaruto <Mouri_Naruto@outlook.com>
2021-03-25 23:58:50 +0800

    Try to fix Ctrl+C issue in the child process.

--- src/MinClr.Init/Program.cs
+++ src/MinClr.Init/Program.cs
@@ -3,50 +3,52 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MinClr.Init
 {
     class Program
     {
-        [DllImport("libc")]
-        private static extern int fork();
+        //[DllImport("libc")]
+        //private static extern int fork();
 
-        [DllImport("libc")]
-        private static extern int execve(
-            string pathname,
-            string[] argv,
-            string[] envp);
+        //[DllImport("libc")]
+        //private static extern int execve(
+        //    string pathname,
+        //    string[] argv,
+        //    string[] envp);
 
-        [DllImport("libc")]
-        private static extern void _exit(
-            int status);
+        //[DllImport("libc")]
+        //private static extern void _exit(
+        //    int status);
 
-        private static int CreateProcessFromInit(
-            ProcessStartInfo startInfo)
-        {
-            int pid = fork();
+        //private static int CreateProcessFromInit(
+        //    ProcessStartInfo startInfo)
+        //{
+        //    int pid = fork();
 
-            if (pid == 0)
-            {
-                Directory.SetCurrentDirectory(startInfo.WorkingDirectory);
+        //    if (pid == 0)
+        //    {
+        //        Directory.SetCurrentDirectory(startInfo.WorkingDirectory);
 
-                List<string> envp = new List<string>();
-                foreach (var item in startInfo.Environment)
-                {
-                    envp.Add(string.Format("{0}={1}", item.Key, item.Value));
-                }
-                envp.Add(null);
+        //        List<string> envp = new List<string>();
+        //        foreach (var item in startInfo.Environment)
+        //        {
+        //            envp.Add(string.Format("{0}={1}", item.Key, item.Value));
+        //        }
+        //        envp.Add(null);
 
-                string[] argv = new string[] { "/usr/bin/bash", null };
+        //        string[] argv = new string[] { "/usr/bin/bash", null };
 
-                execve(argv[0], argv, envp.ToArray());
+        //        execve(argv[0], argv, envp.ToArray());
 
-                // If failed to call execve, it will return the errno.
-                _exit(Marshal.GetLastWin32Error());
-            }
+        //        // If failed to call execve, it will return the errno.
+        //        _exit(Marshal.GetLastWin32Error());
+        //    }
 
-            return pid;
-        }
+        //    return pid;
+        //}
 
 
         [DllImport("libc")]
@@ -66,8 +68,20 @@ namespace MinClr.Init
 
         private const int MNT_DETACH = 2;
 
+        [DllImport("libc")]
+        private static extern int wait(
+            ref int wstatus);
 
+        [DllImport("libc")]
+        private static extern int waitpid(
+            int pid, 
+            ref int wstatus, 
+            int options);
 
+        [DllImport("libc")]
+        private static extern int kill(
+            int pid,
+            int sig);
 
         static void Main(string[] args)
         {
@@ -82,53 +96,66 @@ namespace MinClr.Init
             Console.WriteLine("The version of .NET/Linux: 0.1.2");
             Console.WriteLine("");
 
-            Console.WriteLine("Make / writable.");
-            if (-1 == mount(null, "/", null, MS_REMOUNT, null))
+            Task ExecutorTask = Task.Run(() =>
             {
-                Console.WriteLine("Failed to make / writable.");
-            }
+                Console.WriteLine("Make / writable.");
+                if (-1 == mount(null, "/", null, MS_REMOUNT, null))
+                {
+                    Console.WriteLine("Failed to make / writable.");
+                }
 
-            Console.WriteLine("Detach the /tmp mount point from the ram disk.");
-            if (-1 == umount2("/tmp", MNT_DETACH))
-            {
-                Console.WriteLine(
-                    "Failed to detach the /tmp mount point from the ram disk.");
-            }
+                Console.WriteLine("Detach the /tmp mount point from the ram disk.");
+                if (-1 == umount2("/tmp", MNT_DETACH))
+                {
+                    Console.WriteLine(
+                        "Failed to detach the /tmp mount point from the ram disk.");
+                }
 
-            Console.WriteLine("");
+                Console.WriteLine("");
 
-            Console.WriteLine("Starting DHCP deamon...");
-            try
-            {
-                ProcessStartInfo startInfo = new ProcessStartInfo(
-                    "/usr/sbin/dhcpcd");
-                Process.Start(startInfo).WaitForExit();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Failed to start DHCP deamon. ({0})", e.Message);
-            }
-
-            Console.WriteLine("");
-
-            Console.WriteLine("Starting PowerShell...");
-            for (;;)
-            {
+                Console.WriteLine("Starting DHCP deamon...");
                 try
                 {
                     ProcessStartInfo startInfo = new ProcessStartInfo(
-                        "/opt/minclr/powershell/pwsh",
-                        "-NoLogo");
-                    string OldPath = startInfo.Environment["PATH"];
-                    startInfo.Environment["PATH"] = 
-                        "/opt/minclr/powershell:" + OldPath;
-                    startInfo.Environment["HOME"] = "/root";
-                    startInfo.WorkingDirectory = startInfo.Environment["HOME"];
+                        "/usr/sbin/dhcpcd");
                     Process.Start(startInfo).WaitForExit();
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Failed to start PowerShell. ({0})", e.Message);
+                    Console.WriteLine("Failed to start DHCP deamon. ({0})", e.Message);
+                }
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Starting PowerShell...");
+                for (; ; )
+                {
+                    try
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo(
+                            "/opt/minclr/powershell/pwsh",
+                            "-NoLogo");
+                        string OldPath = startInfo.Environment["PATH"];
+                        startInfo.Environment["PATH"] =
+                            "/opt/minclr/powershell:" + OldPath;
+                        startInfo.Environment["HOME"] = "/root";
+                        startInfo.WorkingDirectory = startInfo.Environment["HOME"];
+                        Process.Start(startInfo).WaitForExit();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Failed to start PowerShell. ({0})", e.Message);
+                    }
+                }
+            });
+
+            while(true)
+            {
+                int wstatus = 0;
+                int pid = wait(ref wstatus);
+                if (pid > 1)
+                {
+                    kill(pid, 0);
                 }
             }
         }
